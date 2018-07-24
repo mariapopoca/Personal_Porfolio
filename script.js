cee81d68aa676c6515a97628e469d691dbc9ef28
Maria <mmhpopoca@gmail.com>
2018-07-24 15:37:52 -0500

    changes

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	
+	console.log("In this website you will learn some facts about me");
+	$("h1").css("text-align", "center");
+});
+
+	$(".hobbies").click(function() {
+		console.log("My favorite hobbies")
+		$ (".hobbies").css("font-size", "25px");
+			
+		});
+	
+$("button").click(function() {
+	console.log("clicked!") ;
+	var name = $("#first_name").val();
+	console.log(name);
+});
+
+	
+
+	
+	
+	
+	
